37d8fef89b4b60cc44e2b533ce556ce334c8fefd
dong-geon-Lee <ch9716@naver.com>
2021-11-21 17:28:57 +0900

    test-01

--- day-1/test/script.js
+++ day-1/test/script.js
@@ -1,1 +1,16 @@
+const panels = document.querySelectorAll(".panel");
 
+console.log(panels);
+
+const removeActivePanel = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActivePanel();
+    panel.classList.add("active");
+  });
+});
