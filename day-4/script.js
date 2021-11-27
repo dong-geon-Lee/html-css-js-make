87dcc2d4e31c63c2be3d588044275f91708b6417
dong-geon-Lee <ch9716@naver.com>
2021-11-27 11:49:45 +0900

    day-4

---
+++ day-4/script.js
@@ -0,0 +1,16 @@
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
+
+// window.addEventListener(
+//   "click",
+//   () => {
+//     search.classList.remove("active");
+//   },
+//   true
+// );
