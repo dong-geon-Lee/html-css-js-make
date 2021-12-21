c557e33b318e1a1f0094609cd721832a689f9941
dong-geon-Lee <ch9716@naver.com>
2021-12-22 00:07:12 +0900

    day-33-34

--- test/day-32/script.js
+++ test/day-32/script.js
@@ -1,1 +1,10 @@
+const toggles = document.querySelectorAll(".toggle");
+const good = document.getElementById("good");
+const cheap = document.getElementById("cheap");
+const fast = document.getElementById("fast");
 
+const clickTrick = () => {};
+
+toggles.forEach((toggle) =>
+  toggle.addEventListener("change", (e) => clickTrick(e.target))
+);
